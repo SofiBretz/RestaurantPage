347f5029ad9e587b7fce54797c9a61e2fdfee7d4
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2020-02-12 11:08:46 -0300

    added the first template to start creating the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const newElement = (elem, parent, id) => {
+    const newElem = document.createElement(elem);
+    const parentElem = document.getElementById(parent);
+    newElem.setAttribute('id', id);
+    parentElem.appendChild(newElem);
+  
+    return id;
+  };
+  newElement('h1', 'content', 'aboutHeader');
+  document.getElementById('aboutHeader').innerHTML = "hello";
